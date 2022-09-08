b2d3b16885fcd5553dafad2776b2490af5a2f2de
Juan Alberto Galih Leo <91479427+juanalberto02@users.noreply.github.com>
2022-09-08 15:11:56 +0700

    Update Kalkulator.py
    Update Kalkulator

--- Kalkulator.py
+++ Kalkulator.py
@@ -1,10 +1,19 @@
-def checkangka(angka):
-    cek = angka.isnumeric()
-    return cek
-
-if __name__ == "__main__":
-    angka = input("Masukkan angka: ")
-    if(checkangka(angka)):
-        print("Anda memasukkan angka ",angka)
-    else:
-        print("Masukkan angka!")
+def kalkulator(angka1,operasi,angka2):
+    while True:
+        if operasi == '+':
+            print('Hasil', angka1 + angka2)
+        elif operasi == '-':
+            print('Hasil', angka1 - angka2)
+        elif operasi == '/':
+            print('Hasil', angka1 / angka2)
+        elif operasi == '*':
+            print('Hasil', angka1 * angka2)
+        else:
+            print('Error')
+            continue
+        break
+    return 
+operator=input("Masukkan jenis operasi (+,-,*,/) :")
+num1=int(input("Masukkan angka pertama :"))
+num2=int(input("Masukkan angka kedua :"))
+kalkulator(num1,operator,num2)
